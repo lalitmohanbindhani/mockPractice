ddd2a58ae0e266dfada2161092d7bef93062bd45
Lalit Mohan Bindhani <bindhanilalitmohan@gmail.com>
2026-02-24 18:07:07 +0530

    call back practice

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-console.log("Hello world");
-console.log("Hwello world");
+////call back function in JS
+
+// function handleEvendListener() {
+//   let count = 0;
+//   document.getElementById("clickMe").addEventListener("click", function () {
+//     console.log("Button Clicked", count++);
+//   });
+// }
+// handleEvendListener();
+
+// setTimeout(function () {
+//   console.log("Hello World");
+// }, 5000);
+
+// function x(y) {
+//   console.log("x");
+//   y();
+// }
+// x(function y() {
+//   console.log("y");
+// });
